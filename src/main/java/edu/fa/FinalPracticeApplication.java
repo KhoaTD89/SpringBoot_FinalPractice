0a97ab30761eb1a4ba2af700277129cbdda0dcd4
KhoaTD <dgkhoa89@gmail.com>
2021-08-06 21:42:00 +0700

    push

---
+++ src/main/java/edu/fa/FinalPracticeApplication.java
@@ -0,0 +1,13 @@
+package edu.fa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalPracticeApplication.class, args);
+    }
+
+}
